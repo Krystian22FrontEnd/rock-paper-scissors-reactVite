5c63507f2d239debb5b25659aac022cb8c4e15fe
Krystian Krupiński <krupinskikrycha@gmail.com>
2025-02-26 13:33:02 +0100

    added new functionalities such as: when player and cpu pick the same then "Tie" will pop up. Results of the game are now shown on top.

--- src/Game/styled.ts
+++ src/Game/styled.ts
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 
+export const Header = styled.h2`
+text-align: center;
+`
+
+export const ResultsWrapper = styled.div`
+display: flex;
+flex-wrap: nowrap;
+justify-content: center;
+`
+
+export const Span = styled.span`
+padding-left: 5px;
+`
+
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding-left: 30px;
 `;
+
+export const TieParagraph = styled.p`
+text-align: center;
+`
 
 export const ButtonWrapper = styled.div`
   display: flex;
@@ -15,5 +34,4 @@ export const Button = styled.button`
   padding: 20px;
   margin: 0 10px;
   width: 100%;
-  max-width: 200px;
 `;
